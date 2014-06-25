3b0317aa4bb5bdc77b5bb2725604a0f086732c82
Bruce Doan <rgv151@gmail.com>
2014-06-26 00:18:33 +0700

    Update vies

---
+++ addons/fuso/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Fuso',
+    'version': '1.0',
+    'category': 'Marketing',
+    'sequence': 2,
+    'summary': 'Fuso',
+    'description': """""",
+    'author': 'Doda100',
+    'website': 'http://www.doda100.com',
+    'depends': [
+        'crm',
+        'mass_mailing',
+    ],
+    'data': [
+        'res_partner_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': [
+        'images/logo.jpg'
+    ],
+}
